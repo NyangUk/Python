4103787b98f5842f2ec5eda2c9f82c7273d2588d
nyanguk <lobgd9150@gmail.com>
2020-02-28 19:58:33 +0900

    2/28

---
+++ 5_1.py
@@ -0,0 +1,29 @@
+#문자열 더하기
+
+a = "hello"
+b = "world"
+print(a+b)
+
+# 문자열 곱하기(문자열의 반복)
+
+print(a*3)
+
+#인덱싱과 슬라이싱
+
+str = "You make me happy"
+print(str[2])
+print(str[15])
+
+
+# 해당 인덱스부터 해당 인덱까지 슬라이싱
+print(str[4: 8])
+
+# 해당 인덱스부터 끝까지 슬라이싱
+print(str[-5:]) 
+print(str[4:])
+# 처음부터 해당 인덱스까지 슬라이싱
+print(str[:3])
+
+# 모두 출력 
+print(str[:])
+
