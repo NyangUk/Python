fc4d5eb508e697bc1e56c1e22b489cedb7b653d7
nyanguk <lobgd9150@gmail.com>
2020-02-29 21:12:09 +0900

    2/29

---
+++ 11_1.py
@@ -0,0 +1,18 @@
+# while문
+
+
+# while <조건> :
+#   ...
+#   ...
+
+a =10
+# while a<14 :
+#     a+=1
+#     print(a) 
+
+while True :
+    a-=1
+    print(a)
+    if not a:
+        print("a = 0 입니다.")
+        break
